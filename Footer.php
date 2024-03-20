2b5831abf99ac29f8cdce9d35f0910d880c718ea
Eddy Lu <eddylu94@users.noreply.github.com>
2024-03-20 09:46:19 -0700

    Update descriptions

--- Footer.php
+++ Footer.php
@@ -1,7 +1,7 @@
             </div>
 
             <div id="footer" style="margin: 30px 0px; text-align: center; color: white;">
-                &copy; 2014-<span id="footer_year"></span> Eddy Lu | <a href="../statistics.php">Statistics</a>
+                &copy; <span id="footer_year"></span> Eddy Lu | <a href="../statistics.php">Statistics</a>
                 <script>
                     var footerYear = document.getElementById("footer_year");
                     footerYear.innerHTML = (new Date()).getFullYear();
